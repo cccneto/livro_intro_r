1358a911f2025ace71b74c78411e1929b1ca413c
Sillas Gonzaga <sillas.gonzaga@gmail.com>
2019-10-01 00:29:47 -0300

    novo capitulo datas

--- config.R
+++ config.R
@@ -1,13 +1,13 @@
-if(!require(checkpoint)){
-  install.packages("checkpoint")
-  library(checkpoint)
-}
-
-checkpoint("2017-11-11")
-
-install.packages('bookdown')
-
-# install.packages('ISLR')
-
-install.packages("webshot")
-webshot::install_phantomjs()
+# if(!require(checkpoint)){
+#   install.packages("checkpoint")
+#   library(checkpoint)
+# }
+#
+# checkpoint("2017-11-11")
+#
+# install.packages('bookdown')
+#
+# # install.packages('ISLR')
+#
+# install.packages("webshot")
+# webshot::install_phantomjs()
